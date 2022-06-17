bcfa4345c51cbb37bac194b8dc12105e895c3290
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-06-17 12:27:44 +0100

    feat: all the formatting

--- frontend/routes/index.tsx
+++ frontend/routes/index.tsx
@@ -5,7 +5,11 @@ import Counter from "../islands/Counter.tsx";
 export default function Home() {
   return (
     <div>
-      <img src="/logo.svg" height="100px" alt="the fresh logo: a sliced lemon dripping with juice" />
+      <img
+        src="/logo.svg"
+        height="100px"
+        alt="the fresh logo: a sliced lemon dripping with juice"
+      />
       <p>
         Welcome to `fresh`. Try update this message in the ./routes/index.tsx
         file, and refresh.
